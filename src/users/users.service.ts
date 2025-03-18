06f30830756c17f7780e11caf40034ae19779769
minwooJung <sae1013@gmail.com>
2025-03-18 23:06:38 +0900

    [FEAT] 회원조회 로직 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,7 +35,8 @@ export class UsersService {
     @Inject(GmailSmtpService) private gmailSmtpService: GmailSmtpService,
     @Inject('REDIS_CLIENT')
     private redisClient: RedisClientType,
-  ) {}
+  ) {
+  }
 
   async create(newUser: CreateUserDto) {
     const duplicatedUser = await this.userRepository
@@ -104,8 +105,8 @@ export class UsersService {
   }
 
   async requestPasswordResetVerificationCode({
-    email,
-  }: RequestPasswordResetVerificationCodeDto) {
+                                               email,
+                                             }: RequestPasswordResetVerificationCodeDto) {
     await this.getUserProfileByEmail(email);
     const redisAuthCodeKey = `passwordReset:authCode:${email}`;
     const redisAuthCode = await this.redisClient.get(redisAuthCodeKey);
@@ -180,8 +181,14 @@ export class UsersService {
     await this.redisClient.del(redisAuthCodeKey);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.membership', 'membership')
+      .orderBy('user.createdAt', 'DESC')
+      .getMany();
+    return users;
+
   }
 
   // findOne(userInfo: FindUserByEmailDto) {
@@ -193,5 +200,6 @@ export class UsersService {
   // }
 
   // 유저정보를 토큰에서 빼온다음...
-  remove() {}
+  remove() {
+  }
 }
